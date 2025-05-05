222643aed1f9b035700932bb92850bdc4795c64e
ingDiegoCalderon <ingdiegocalderon@ingDiegoCalderons-MacBook-Pro.local>
2025-05-05 11:19:05 -0500

    Termine la entidad de productos

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,5 +1,40 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
-export class Product {}
+@Entity()
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
 
-@Entity
+    @Column('text', {
+        unique: true,
+    })
+    title: string;
+
+    @Column('numeric',{
+        default: 0,
+    })
+    price: number;
+
+    @Column('text', {
+      nullable: true,  
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true,
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0,
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true,
+    })
+    sizes: string[];
+
+    @Column('text')
+    gender: string;
+}
