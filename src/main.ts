c8fecacd14df56b2e37aea5247b0d2bc3b06b6ec
ingDiegoCalderon <ingdiegocalderon@ingDiegoCalderons-MacBook-Pro.local>
2025-05-05 11:34:35 -0500

    Cree el DTO de products

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+// configurar la ruta
+  app.setGlobalPrefix('api');
+// configurar la validacion de Pipes para el DTO
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  )
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
